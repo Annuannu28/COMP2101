7b581fac9572d17b043c3f2891b9e6c396e1d68e
annu <you@yourdomain>
2022-10-07 18:07:09 +0000

    your commit comment

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 # My challenge script - sysinfo.sh
-# The system’s fully-qualified domain name (FQDN)
-hostname -A
+# Embedded variables for report data
 
-# The operating system name and version
-hostnamectl
+FQDN1=$(hostname)
+OS_NAME=$(cat /etc/os-release | grep -w NAME)
+VERSION_NAME=$(cat /etc/os-release | grep -w VERSION)
+IP_ADD=$(ip a s enp0s8 | grep -w inet | awk '{print $2}')
+Filesystem_Freespace=$(df -h  --output=avail / | grep Avail -v)
+# print out the report using the gathered data
+cat <<EOF
+Report for myvm
+===============
+FQDN: $FQDN1
+Operating system name and version: $OS_NAME/$VERSION_NAME
+IP Address: $IP_ADD
+Root Filesystem Free Space: $Filesystem_Freespace
+============== 
+EOF
 
-# Showing the IP addresses
-hostname -I
 
-# Space available in only the root filesystem
-df -h /
+
